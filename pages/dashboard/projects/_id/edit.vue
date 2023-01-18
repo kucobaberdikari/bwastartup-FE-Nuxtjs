7998099441e1ed333256121225d258ec51b6b598
kucobaberdikari <57296125+kucobaberdikari@users.noreply.github.com>
2023-01-18 15:14:51 +0700

    integrasi API full

--- pages/dashboard/projects/_id/edit.vue
+++ pages/dashboard/projects/_id/edit.vue
@@ -13,7 +13,9 @@
         </div>
         <div class="flex justify-between items-center">
           <div class="w-3/4 mr-6">
-            <h3 class="text-2xl text-gray-900 mb-4">Create New Projects</h3>
+            <h3 class="text-2xl text-gray-900 mb-4">
+              Edit Campaign "{{ campaign.data.name }}"
+            </h3>
           </div>
           <div class="w-1/4 text-right">
             <button
@@ -38,10 +40,10 @@
                       Campaign Name
                     </label>
                     <input
-                    class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                    type="text"
-                    placeholder="Contoh: Demi Gunpla Demi Istri"
-                    v-model="campaign.name"
+                      class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                      type="text"
+                      placeholder="Contoh: Demi Gunpla Demi Istri"
+                      v-model="campaign.data.name"
                     />
                   </div>
                   <div class="w-full md:w-1/2 px-3">
@@ -54,7 +56,7 @@
                       class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                       type="number"
                       placeholder="Contoh: 200000"
-                      v-model.number="campaign.goal_amount"
+                      v-model.number="campaign.data.goal_amount"
                     />
                   </div>
                   <div class="w-full px-3">
@@ -67,7 +69,7 @@
                       class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                       type="text"
                       placeholder="Deskripsi singkat mengenai projectmu"
-                      v-model="campaign.short_description"
+                      v-model="campaign.data.short_description"
                     />
                   </div>
                   <div class="w-full px-3">
@@ -80,7 +82,7 @@
                       class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                       type="text"
                       placeholder="Contoh: Ayam, Nasi Goreng, Piring"
-                      v-model="campaign.perks"
+                      v-model="campaign.data.perks"
                     />
                   </div>
                   <div class="w-full px-3">
@@ -93,7 +95,7 @@
                       class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                       type="text"
                       placeholder="Isi deskripsi panjang untuk projectmu"
-                      v-model="campaign.description"
+                      v-model="campaign.data.description"
                     ></textarea>
                   </div>
                 </div>
@@ -106,139 +108,138 @@
       <section class="call-to-action bg-purple-progress pt-64 pb-10"></section>
       <Footer />
     </div>
-</template>
-
-<script>
-export default {
-  middleware: 'auth',
-  data() {
-    return {
-      campaign: {
-        name: '',
-        short_description: '',
-        description: '',
-        // goal_amount: 0,
-        perks: '',
-      },
-    }
-  },
-  methods: {
-    async save() {
-      try {
-        let response = await this.$axios.$post(
-          '/api/v1/campaigns',
-          this.campaign
-        )
-        this.$router.push({
-          name: 'dashboard-projects-id',
-          params: { id: response.data.id },
-        })
-        console.log(response)
-      } catch (err) {
-        console.log(err)
-      }
+  </template>
+  
+  <script>
+  export default {
+    middleware: 'auth',
+    async asyncData({ $axios, params }) {
+      const campaign = await $axios.$get('/api/v1/campaigns/' + params.id)
+      return { campaign }
     },
-  },
-}
-</script>
-
-<!-- <style lang="scss">
-.dashboard-header {
-  background-image: url('/auth-background.svg');
-  background-position: top right;
-  background-repeat: no-repeat;
-  background-color: #3b41e3;
-  background-size: 250px;
-  height: 100px;
-}
-
-.cta-clip {
-  position: relative;
-  top: 200px;
-  bottom: 0;
-  right: 0;
-  left: 0;
-  width: 100%;
-  height: 300px;
-  background-position: top right;
-  background-size: 300px;
-  background-repeat: no-repeat;
-  background-color: #fff;
-  transform: skewY(4deg);
-  transform-origin: bottom right;
-}
-
-.call-to-action {
-  background-image: url('/auth-background.svg');
-  background-position: top right;
-  background-repeat: no-repeat;
-  background-size: 450px;
-}
-
-.card-project {
-  transition: all 0.3s ease 0s, opacity 0.5s cubic-bezier(0.5, 0, 0, 1) 1ms;
-  max-height: 445px;
-  overflow: hidden;
-
-  .button-cta {
-    opacity: 0;
-    transition: all 300ms ease;
+    methods: {
+      async save() {
+        try {
+          let response = await this.$axios.$put(
+            '/api/v1/campaigns/' + this.$route.params.id,
+            {
+              name: this.campaign.data.name,
+              short_description: this.campaign.data.short_description,
+              description: this.campaign.data.description,
+              goal_amount: this.campaign.data.goal_amount,
+              perks: this.campaign.data.perks.join(),
+            }
+          )
+          console.log(response)
+        } catch (err) {
+          console.log(err)
+        }
+      },
+      changeImage(url) {
+        this.default_image = url
+      },
+    },
   }
-
-  &:hover {
-    box-shadow: 0 4px 25px 0 rgba(0, 0, 0, 0.15);
-
+  </script>
+  
+  <!-- <style lang="scss">
+  .dashboard-header {
+    background-image: url('/auth-background.svg');
+    background-position: top right;
+    background-repeat: no-repeat;
+    background-color: #3b41e3;
+    background-size: 250px;
+    height: 100px;
+  }
+  
+  .cta-clip {
+    position: relative;
+    top: 200px;
+    bottom: 0;
+    right: 0;
+    left: 0;
+    width: 100%;
+    height: 300px;
+    background-position: top right;
+    background-size: 300px;
+    background-repeat: no-repeat;
+    background-color: #fff;
+    transform: skewY(4deg);
+    transform-origin: bottom right;
+  }
+  
+  .call-to-action {
+    background-image: url('/auth-background.svg');
+    background-position: top right;
+    background-repeat: no-repeat;
+    background-size: 450px;
+  }
+  
+  .card-project {
+    transition: all 0.3s ease 0s, opacity 0.5s cubic-bezier(0.5, 0, 0, 1) 1ms;
+    max-height: 485px;
+    overflow: hidden;
+  
     .button-cta {
-      opacity: 1;
-      transition: all 300ms ease;
-    }
-
-    .progress-bar,
-    .progress-info {
       opacity: 0;
-      height: 0px;
-      margin: 0px;
-      padding: 0px;
       transition: all 300ms ease;
     }
+  
+    &:hover {
+      box-shadow: 0 4px 25px 0 rgba(0, 0, 0, 0.15);
+  
+      .button-cta {
+        opacity: 1;
+        transition: all 300ms ease;
+      }
+  
+      .progress-bar,
+      .progress-info {
+        opacity: 0;
+        height: 0px;
+        margin: 0px;
+        padding: 0px;
+        transition: all 300ms ease;
+      }
+    }
   }
-}
-
-footer {
-  z-index: inherit;
-}
-
-.hero-underline {
-  text-decoration-color: #1abc9c;
-}
-
-.testimonial-user {
-  opacity: 0.4;
-  &.active {
-    opacity: 1;
-    border: 5px solid #fff;
-    box-shadow: 0 0 0 1px #3b41e3;
+  
+  footer {
+    z-index: inherit;
   }
-}
-
-.list-check {
-  li {
-    background: url('/icon-checklist.svg') no-repeat left 8px;
-    padding: 6px 0px 3px 28px;
+  
+  .hero-underline {
+    text-decoration-color: #1abc9c;
   }
-}
-
-.item-thumbnail:hover {
-  background-color: #ff872e;
-  border-radius: 20px;
-  &:after {
-    position: absolute;
-    top: 38%;
-    left: 41%;
-    content: url('/icon-thumbnail-hover.svg');
+  
+  .testimonial-user {
+    opacity: 0.4;
+    &.active {
+      opacity: 1;
+      border: 5px solid #fff;
+      box-shadow: 0 0 0 1px #3b41e3;
+    }
   }
-  img {
-    opacity: 0.3;
+  
+  .list-check {
+    li {
+      background: url('/icon-checklist.svg') no-repeat left 8px;
+      padding: 6px 0px 3px 28px;
+    }
   }
-}
-</style> -->
+  
+  .item-thumbnail:hover {
+    background-color: #ff872e;
+    border-radius: 20px;
+    &:after {
+      position: absolute;
+      top: 38%;
+      left: 41%;
+      content: url('/icon-thumbnail-hover.svg');
+    }
+    img {
+      opacity: 0.3;
+    }
+  }
+  </style> -->
+  
